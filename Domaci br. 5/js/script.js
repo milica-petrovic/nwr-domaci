87f9baf4a7dfbd208a351b876535f8f62dfc05ce
Milica Petrovic <petrovic.milica598@gmail.com>
2022-05-19 18:39:39 +0200

    5.zadatak

--- Domaci br. 5/js/script.js
+++ Domaci br. 5/js/script.js
@@ -5,20 +5,20 @@
 // ? na stranici kao listu
 
 
-let polaznici = [
-    {
-        name: 'Milica Petrovic',
-        score: 90
-    },
-    {
-        name: 'Lazar Dinic',
-        score:30
-    },
-    {
-        name: 'Ilic Zorana',
-        score: 50
-    }
-]
+// let polaznici = [
+//     {
+//         name: 'Milica Petrovic',
+//         score: 90
+//     },
+//     {
+//         name: 'Lazar Dinic',
+//         score:30
+//     },
+//     {
+//         name: 'Ilic Zorana',
+//         score: 50
+//     }
+// ]
 // let niz = polaznici.map(polaznik =>{
 //     if(polaznik.score < 50){
 //         return{
@@ -41,14 +41,12 @@ let polaznici = [
 // ? POMOC: potraziti funkciju za konvertovanje stringa u velika slova 
 // ? REZULTAT: ['MILICA PETROVIC', 'LAZAR DINIC']
 
-let niz = polaznici.map(polaznik =>{
-    let name.toUpperCase()
-    }
-}) 
+// let polaznici = ['Milica Petrovic', 'Lazar Dinic']
 
-niz.forEach(polaznik=>{
-    document.write(`<li>${polaznik.name}</li>`)
-})
+// let niz = polaznici.map(elementPolaznici =>{
+//     return elementPolaznici.toUpperCase()
+// })
+// console.log(niz)
 
 
 
@@ -58,14 +56,47 @@ niz.forEach(polaznik=>{
 // ? nego niz name stringova
 // ? REZULTAT: ['Milica Petrovic', 'Lazar Dinic']
 
+// let polaznici = [
+//     {
+//         name: 'Milica Petrovic',
+//         age: 23
+//     },
+//     {
+//          name: 'Lazar Dinic',
+//         age: 26
+//     }
+// ]
+// let niz = polaznici.map((elementNiza) => {
+//     return Object.values(elementNiza)[0]
+// })
+// console.log(niz)
+
 // * ZADATAK 4
 // ? Mapirati niz brojeva tako da dobijemo novi niz
 // ? brojeva koji su pomnozeni brojem 2
 // ? PRIMER: [1,2,3,4]
 // ? REZULTAT: [2,4,6,8]
 
+// let niz = [1,2,3,4]
+
+// let niz2 = niz.map((broj) =>{
+//     return broj*2
+// })
+
+// console.log(niz2)
+
+
 // * ZADATAK 5
 // ? Mapirati niz brojeva tako da dobijemo niz stringova
 // ? PRIMER: [1,2,3,4]
 // ? REZULTAT: ['1','2','3','4']
 // ? POMOC: Potraziti f-ju za konvertovanje u string
+
+
+let niz = [1,2,3,4]
+
+let niz2 = niz.map((broj) =>{
+    return broj.toString()
+})
+
+console.log(niz2)
